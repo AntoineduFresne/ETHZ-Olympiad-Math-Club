b5fc5d2b4e7b1816f7ff145e8b303f3c925d2372
AntoineduFresne <antoine@du-fresne.ch>
2025-09-24 12:54:04 +0200

    Local changes

--- convert_tex_to_pdf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-find static/uploads/problem -name '*.tex' | while read texfile; do
-    pdfdir=$(dirname "$texfile")
-    cd "$pdfdir"
-    latexmk -pdf -silent "$(basename "$texfile")"
-    cd -
-done
